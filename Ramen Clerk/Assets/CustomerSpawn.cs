b6b03c9710a35e7a6df256e76fd0dd9033780b7f
ukojima <22211142rk@tama.ac.jp>
2024-07-17 12:28:44 +0900

    列を作る

--- Ramen Clerk/Assets/CustomerSpawn.cs
+++ Ramen Clerk/Assets/CustomerSpawn.cs
@@ -10,6 +10,8 @@ public class CustomerSpawn : MonoBehaviour
     int x =188;
     int y =1;
     int z = 13;
+    public int min;
+    public int max;
 
     // Use this for initialization
     void Start () 
@@ -17,13 +19,15 @@ public class CustomerSpawn : MonoBehaviour
         time = 1.0f;
     }
 
+    
+
     void Update()
     {
         time -= Time.deltaTime;
         if(time < 0.0f)
         {
             time = 1.0f;
-            number = Random.Range(0, 10);
+            number = Random.Range(min, max);
             if(number == 0)
             {
                 Instantiate (Customer[0], new Vector3(x,y,z), Quaternion.identity);
